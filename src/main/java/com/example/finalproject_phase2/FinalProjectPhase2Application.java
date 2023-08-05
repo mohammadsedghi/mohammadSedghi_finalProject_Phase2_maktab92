ffd8df936241c48c19646c220d485da3aa32a540
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-08-05 21:36:31 +0330

    project phase2 is created

---
+++ src/main/java/com/example/finalproject_phase2/FinalProjectPhase2Application.java
@@ -0,0 +1,13 @@
+package com.example.finalproject_phase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectPhase2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectPhase2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalproject_phase2/FinalProjectPhase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalproject_phase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectPhase2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
