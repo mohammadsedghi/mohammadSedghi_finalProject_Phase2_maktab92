d1db795d9803afe130981c724a31911d0cb119b5
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-08-05 22:53:16 +0330

    BaseEntity.java is written

---
+++ src/main/java/com/example/finalproject_phase2/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.example.finalproject_phase2;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@MappedSuperclass
+public class BaseEntity <ID extends Serializable>implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private ID id;
+}
