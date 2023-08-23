8059883368efe746bb4db152c6f5a40342219d58
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-08-24 02:44:08 +0330

    captcha is written  ,and wrong item in front is corrected

--- src/main/java/com/example/finalproject_phase2/controller/WalletController.java
+++ src/main/java/com/example/finalproject_phase2/controller/WalletController.java
@@ -3,106 +3,105 @@ package com.example.finalproject_phase2.controller;
 import com.example.finalproject_phase2.entity.Customer;
 import com.example.finalproject_phase2.service.CustomerService;
 import com.example.finalproject_phase2.service.WalletService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/wallet")
 public class WalletController {
-
+    String captchaText;
     private final WalletService walletService;
-
     private final CustomerService customerService;
-
     @Autowired
     public WalletController(WalletService walletService, CustomerService customerService) {
         this.walletService = walletService;
         this.customerService = customerService;
     }
-
-//    @RequestMapping("/email")
-//    public ModelAndView getUser(@PathVariable String email) {
-//        Customer user = customerService.findByEmail(email).get();
-//        ModelAndView modelAndView = new ModelAndView("userView");
-//        modelAndView.addObject("user", user);
-//        return modelAndView;
-//    }
-   @RequestMapping(value = "/payment",method = RequestMethod.GET)
-    public String home(){
+    @RequestMapping(value = "/payment", method = RequestMethod.GET)
+    public String home() {
         return "index";
     }
-    @GetMapping ( "/send-data")
+
+    @PostMapping("/send-data")
     @ResponseBody
-    public String sendData(@RequestParam String data ,@RequestParam String data2 , Model model) {
+    public String sendData(@RequestParam String cardNumber1, @RequestParam String cardNumber2
+            , @RequestParam String cardNumber3, @RequestParam String cardNumber4, @RequestParam String cvv2,
+                           @RequestParam String month, @RequestParam String year,
+                           @RequestParam String captcha, @RequestParam String password, Model model) {
         String response;
-        if (data != null && !data.isEmpty()&& data2 != null && !data2.isEmpty()) {
-            System.out.println(data);
-            System.out.println(data2);
-            response = "Received data 1: " + data + ", Received data 2: " + data2;
+        if (cardNumber1 != null && !cardNumber1.isEmpty() && cardNumber2 != null && !cardNumber2.isEmpty()) {
+            System.out.println(cardNumber1);
+            System.out.println(cardNumber2);
+            response = "Received data 1: " + cardNumber1 + ", Received data 2: " + cardNumber2;
         } else {
             return "No data received.";
         }
         model.addAttribute("response", response);
-        return "index :: #response";
+        return captchaText;
+//        return "index :: #response";
     }
 
-//    @PostMapping ( "/send-data")
-//    @ResponseBody
-//    public String sendData(@RequestBody DataPayload  payload  , Model model) {
-//        String response;
-//        if (payload.getData1() != null && !payload.getData1().isEmpty()
-//                && payload.getData2() != null && !payload.getData2().isEmpty()) {
-//            // Process the data as needed
-//            System.out.println(payload.getData1());
-//            System.out.println(payload.getData2());
-//            response = "Received data 1: " + payload.getData1() + ", Received data 2: " + payload.getData2();
-//        } else {
-//            response = "Incomplete data received.";
-//        }
-//
-//        model.addAttribute("response", response);
-//        return "index :: #response";
-//    }
-//
-//    public static class DataPayload {
-//        private String data1;
-//        private String data2;
-//
-//        // Getters and setters
-//
-//        public String getData1() {
-//            return data1;
-//        }
-//
-//        public void setData1(String data1) {
-//            this.data1 = data1;
-//        }
-//
-//        public String getData2() {
-//            return data2;
-//        }
-//
-//        public void setData2(String data2) {
-//            this.data2 = data2;
-//        }
-//    }
+    @GetMapping("/captcha")
+    public void captcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String captchaText = generateCaptchaText(6); // 6 characters
+        this.captchaText=captchaText;
+        // Store the CAPTCHA text in the user's session
+        HttpSession session = request.getSession();
+        session.setAttribute("captcha", captchaText);
 
+        // Create an image with the CAPTCHA text
+        BufferedImage captchaImage = generateCaptchaImage(captchaText);
 
+        // Send the image to the client
+        response.setContentType(MimeTypeUtils.IMAGE_PNG_VALUE);
+        OutputStream out = response.getOutputStream();
+        ImageIO.write(captchaImage, "png", out);
+        out.close();
+    }
 
-//@RequestMapping(value = "/payment",method = RequestMethod.GET)
-//public String showForm( @ModelAttribute("customer") Customer customer) {
-//   // Customer user = customerService.findByEmail(email).get();
-//   // ModelAndView modelAndView = new ModelAndView("index");
-//      // modelAndView.addObject("user", user);
-//    return "/WEB-INF/payment.jsp";
-//}
-//    @RequestMapping("/email")
-//    public String getUser( String email) {
-//      //  Customer user = customerService.findByEmail(email).get();
-//     //   return user.getEmail();
-//        return "email";
-//    }
+    private String generateCaptchaText(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder captchaText = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            captchaText.append(characters.charAt(index));
+        }
+        return captchaText.toString();
+    }
+
+    private BufferedImage generateCaptchaImage(String text) {
+        int width = 150;
+        int height = 50;
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = image.createGraphics();
+
+        // Set background color
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, width, height);
+
+        // Draw text
+        g2d.setColor(Color.BLACK);
+        g2d.setFont(new Font("Arial", Font.BOLD, 30));
+        g2d.drawString(text, 30, 35);
+
+        g2d.dispose();
+        return image;
+    }
 }
+
+
